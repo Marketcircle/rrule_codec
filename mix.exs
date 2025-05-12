65c71edec415abb8e0abdd8ef0639b00220fd9e8
Chris Hopkins <15328901+chrishop@users.noreply.github.com>
2025-05-12 18:11:45 +0900

    chore: set version to 0.1.1 as its first release of new project

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule RruleParser.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/team-alebmic/rrule_parser"
-  @version "1.2.0"
+  @version "0.1.1"
 
   def project do
     [
