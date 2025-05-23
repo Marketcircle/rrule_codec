e7d8131399be9cd4c16d2e86c22d42e8ef427e5a
Chris Hopkins <15328901+chrishop@users.noreply.github.com>
2025-05-23 14:27:30 +0900

    chore: include checksum to verify binary integrity

---
+++ checksum-Elixir.RruleCodec.Rrule.Api.exs
@@ -0,0 +1,8 @@
+%{
+  "librrule_codec_rs-v0.1.6-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:bfd4f437f6e6b6de53c4d0ba8f1b25f31100f532fa4be1525533ef06718644f3",
+  "librrule_codec_rs-v0.1.6-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:34838a1cde227b1df59fbd23ce974f5962fba4a1e7376f8cb7b2b249aa703204",
+  "librrule_codec_rs-v0.1.6-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:8dac786e3c535ffc978a0f7b637bb112d89ae4522048b28f13da688786f90efd",
+  "librrule_codec_rs-v0.1.6-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:032fed1df2749dd8b7823e57f3a122ae1509523a26087f8c27fe4b5d91fe32d1",
+  "librrule_codec_rs-v0.1.6-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:1930087bea70f23b1be450ccc3ccd1365731eb7886698b5f11e58e480a752d05",
+  "librrule_codec_rs-v0.1.6-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:d1efaffe5b05e56c4203cec4fc6c25a9f41bff5b4d74f88de19c52e8917f6ef4",
+}
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule RruleCodec.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/Marketcircle/rrule_codec"
-  @version "0.1.6"
+  @version "0.1.7"
 
   def project do
     [
