6d35d073d80382b58119ba24eb37fd0d696a751c
Chris Hopkins <15328901+chrishop@users.noreply.github.com>
2025-05-23 13:47:17 +0900

    chore new ver 0.1.4

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule RruleCodec.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/team-alebmic/rrule_codec"
-  @version "0.1.3"
+  @version "0.1.4"
 
   def project do
     [
