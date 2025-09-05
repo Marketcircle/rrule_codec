0421d6bc3bf88b60fb48cd4869efe337fca000e4
Adam Tharani <adamtharani@me.com>
2025-09-05 11:08:19 -0400

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule RruleCodec.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/Marketcircle/rrule_codec"
-  @version "0.1.8"
+  @version "0.1.9"
 
   def project do
     [
